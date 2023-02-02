6fabdc668f927b8cacfee8b8fc65614b06c24adb
Scott <scott.ti312@gmail.com>
2023-02-01 18:00:55 -0800

    - Add high score message - Change cards from 0 - 11 to 1 - 12

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useSyncExternalStore } from 'react';
 import CardGrid from './CardGrid';
 import Rules from './Rules';
 import { shuffleArray } from './utils';
 import styled from 'styled-components';
 
 function App() {
-  const [cards, setCards] = useState([...Array(12).keys()]);
+  const [cards, setCards] = useState([...Array(12).keys()].map(i => i + 1));
   const [memory, setMemory] = useState([]);
   const [score, setScore] = useState(0);
   const [highScore, setHighScore] = useState(0);
+  const [highScoreMet, setHighScoreMet] = useState(false);
+  const [lost, setLost] = useState(false);
 
   useEffect(() => {
     setCards(shuffleArray(cards));
@@ -22,6 +24,7 @@ function App() {
       let newScore = score + 1;
       if (newScore > highScore) {
         setHighScore(newScore);
+        setHighScoreMet(true);
       }
       setScore(newScore);
       setMemory([...memory, chosenNumber]);
@@ -31,9 +34,15 @@ function App() {
   };
 
   const loseScreen = () => {
+    setLost(true);
+  }
+  
+  const restart = () => {
+    setCards(shuffleArray(cards))
     setScore(0);
     setMemory([]);
-    console.log('You lose!');
+    setLost(false);
+    setHighScoreMet(false);
   }
 
   return (
@@ -41,9 +50,18 @@ function App() {
       <div>High Score: {highScore}</div>
       <div>Score: {score}</div>
 
-      <CardGrid cardArray={cards} handleCardClick={handleCardClick}/>
-      <RestartButton onClick={loseScreen}>Restart</RestartButton>
-      <Rules/>
+      <CardGrid 
+        cardArray={cards}
+        handleCardClick={lost ? undefined : handleCardClick}
+      />
+      {lost && (
+        <>
+          <div>You lose.</div>
+          {highScoreMet && (<div>New high score!</div>)}
+          <RestartButton onClick={restart}>Restart</RestartButton>
+        </>
+      )}
+      {!lost && (<Rules/>)}
     </Container>
   );
 }
